33abceee1de1ffae24960bdc42ea9068aeb64299
Fadil Smajilbasic <fadil.smajilbasic@samtrevano.ch>
2019-09-24 16:28:38 +0200

    added database creation script

--- Code/databaseCreation.sql
+++ Code/databaseCreation.sql
@@ -1,55 +1,65 @@
-CREATE DATABASE Deduplicator;
-USE Deduplicator;
+CREATE DATABASE deduplicator;
+USE deduplicator;
 
-CREATE TABLE Deduplicator.tipoAzione
+CREATE TABLE deduplicator.action_type
 (
-    tipo VARCHAR(50) NOT NULL PRIMARY KEY
+    type VARCHAR(50) NOT NULL PRIMARY KEY
     
 );
 
-CREATE TABLE Deduplicator.azioni
-(
-    percorsoFile VARCHAR(255) not null primary key,
-    tipoAzione VARCHAR(50),
-    FOREIGN KEY(tipoAzione) REFERENCES tipoAzione(tipo)
-    
+CREATE TABLE deduplicator.authentication_details(
+    username varchar(50) not null primary key,
+    password varchar(640) 
+);
+
+CREATE TABLE deduplicator.scheduler(
+    id int not null primary key,
+    monthly int default null,
+    weekly tinyint default null,
+    hour int,
+    repeated boolean,
+    date_start date
+
 );
 
 
-
-CREATE TABLE Deduplicator.tipoPercorso
+CREATE TABLE deduplicator.action
 (
-    tipo VARCHAR(50) NOT NULL PRIMARY KEY
-    
+    id int not null primary key,
+    filepath VARCHAR(255),
+    new_filepath VARCHAR(255) default null,
+    action_type VARCHAR(50) not null,
+    executed boolean,
+    date_added timestamp,
+    user varchar(50),
+    scheduler int null,
+    FOREIGN KEY(action_type) REFERENCES action_type(type),
+    FOREIGN key(user) REFERENCES authentication_details(username),
+    FOREIGN key(scheduler) REFERENCES scheduler(id)
+);
+
+CREATE TABLE deduplicator.report(
+    id int not null primary key,
+    duration int,
+    start timestamp,
+    duplicate_count int,
+    user varchar(50),
+    FOREIGN KEY(user) REFERENCES authentication_details(username)
 );
 
 
-CREATE TABLE Deduplicator.percorsi
-(
-    percorso varchar(255) not null PRIMARY key, -- primary key column
-    file boolean NOT null,
-    date TIMESTAMP not NULL,
-    tipoPercorso VARCHAR(50),
-    FOREIGN KEY(tipoPercorso) REFERENCES tipoPercorso(tipo)
-    
+CREATE TABLE deduplicator.file(
+    path varchar(255) not null primary key,
+    last_modified timestamp not null,
+    hash varchar(16),
+    size int,
+    report_id int,
+    FOREIGN key(report_id) REFERENCES report(id)
 );
 
-
-CREATE TABLE Deduplicator.rapporti
-(
-    timestamp TIMESTAMP NOT NULL PRIMARY KEY, -- primary key column
-    n_duplicati SMALLINT NOT NULL,
-    origin VARCHAR(50)
-);
-
-
-CREATE TABLE Deduplicator.file
-(
-    percorso varchar(255) NOT NULL PRIMARY KEY, -- primary key column
-    dataModifica date not null,
-    hash BINARY(16) not null,
-    grandezza BIGINT not null,
-    rapporto TIMESTAMP,
-    FOREIGN KEY(rapporto) REFERENCES rapporti(timestamp)
-   
+CREATE TABLE deduplicator.global_path(
+    path varchar(255) not null primary key,
+    file BOOLEAN,
+    ignore_file BOOLEAN,
+    date timestamp
 );
