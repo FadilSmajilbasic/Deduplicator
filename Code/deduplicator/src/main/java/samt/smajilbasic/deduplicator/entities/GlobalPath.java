3ec7fabf2bcf56393e38341b99292b3c049b621a
Fadil Smajilbasic <fadil.smajilbasic@samtrevano.ch>
2019-10-03 16:25:43 +0200

    update global path

--- Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/entities/GlobalPath.java
+++ Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/entities/GlobalPath.java
@@ -1,20 +1,36 @@
 package samt.smajilbasic.deduplicator.entities;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Timestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import samt.smajilbasic.deduplicator.exception.PathException;
+
 @Entity
 public class GlobalPath {
 	@Id
-    private String path;
+	private String path;
 
-    private boolean file;
+	private boolean file;
 
 	private boolean ignoreFile;
-	
+
 	private Timestamp date;
+
+	public GlobalPath() {
+	}
+
+	public GlobalPath(String path, boolean ignoreFile) {
+		this.setPath(path);
+		this.setIgnoreFile(ignoreFile);
+		this.setDate(new Timestamp(System.currentTimeMillis()));
+	}
 
 	/**
 	 * @return the path
@@ -26,10 +42,33 @@ public class GlobalPath {
 	/**
 	 * @param path the path to set
 	 */
-	public void setPath(String path) {
-		this.path = path;
+	private void setPath(String path) {
+		try {
+			if (path != null) {
+
+				String pathParsed = path.replaceAll("&#47;", File.separator);
+				System.out.println("Path: " + pathParsed);
+				Path pa = Paths.get(pathParsed);
+
+				this.path = path;
+				if (Files.isDirectory(pa)) {
+					this.setFile(false);
+				} else {
+					if (Files.exists(pa)) {
+						this.setFile(true);
+					} else {
+						throw new InvalidPathException(path, "Doesn't exist");
+					}
+				}
+
+			} else {
+				throw new PathException();
+			}
+		} catch (InvalidPathException | NullPointerException | SecurityException ex) {
+			System.err.println("[ERROR] " + ex.getMessage());
+			throw new PathException(ex.getMessage());
+		}
 	}
-	
 
 	/**
 	 * @return the file
@@ -41,7 +80,7 @@ public class GlobalPath {
 	/**
 	 * @param file the file to set
 	 */
-	public void setFile(boolean file) {
+	private void setFile(boolean file) {
 		this.file = file;
 	}
 
@@ -55,10 +94,9 @@ public class GlobalPath {
 	/**
 	 * @param ignoreFile the ignoreFile to set
 	 */
-	public void setIgnoreFile(boolean ignoreFile) {
+	private void setIgnoreFile(boolean ignoreFile) {
 		this.ignoreFile = ignoreFile;
 	}
-
 
 	/**
 	 * @return the date
@@ -70,7 +108,7 @@ public class GlobalPath {
 	/**
 	 * @param date the date to set
 	 */
-	public void setDate(Timestamp date) {
+	private void setDate(Timestamp date) {
 		this.date = date;
 	}
 }
