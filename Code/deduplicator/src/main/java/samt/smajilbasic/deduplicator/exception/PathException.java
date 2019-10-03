940544065dc732495b7d3ca4e10c5217ebc6f5bd
Fadil Smajilbasic <fadil.smajilbasic@samtrevano.ch>
2019-10-03 16:26:19 +0200

    added path exception

---
+++ Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/exception/PathException.java
@@ -0,0 +1,15 @@
+package samt.smajilbasic.deduplicator.exception;
+
+/**
+ * InvalidPathException
+ */
+public class PathException extends RuntimeException{
+
+    public PathException() {
+        super();
+    }
+
+    public PathException(String message) {
+        super(message);
+    }
+}
