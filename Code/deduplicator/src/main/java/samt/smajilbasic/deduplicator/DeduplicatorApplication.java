dde2ccb28d1b238083f096ed0ca810340e88eea1
Fadil Smajilbasic <fadil.smajilbasic@samtrevano.ch>
2019-10-05 10:02:51 +0200

    Added packages to scan to DeduplicatorApplication

--- Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/DeduplicatorApplication.java
+++ Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/DeduplicatorApplication.java
@@ -3,7 +3,7 @@ package samt.smajilbasic.deduplicator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "samt.smajilbasic.deduplicator.controller", "samt.smajilbasic.deduplicator.repository","samt.smajilbasic.deduplicator.entity"})
 public class DeduplicatorApplication {
 
 	public static void main(String[] args) {
