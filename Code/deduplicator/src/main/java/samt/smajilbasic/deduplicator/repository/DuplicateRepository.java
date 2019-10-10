35d9c7089c064931a3e54ec741d2e5ffd557820a
Fadil Smajilbasic <fadil.smajilbasic@samtrevano.ch>
2019-10-10 16:27:19 +0200

    Added new repository

---
+++ Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/repository/DuplicateRepository.java
@@ -0,0 +1,25 @@
+package samt.smajilbasic.deduplicator.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import samt.smajilbasic.deduplicator.entity.Report;
+import samt.smajilbasic.deduplicator.entity.Duplicate;
+
+/**
+ * DuplicateRepository
+ */
+@Repository
+public interface DuplicateRepository extends CrudRepository<Duplicate, String> {
+    @Query("SELECT " +
+           "    new samt.smajilbasic.deduplicator.entity.Duplicate(f.path, f.lastModified) " +
+           "FROM " +
+           "    File f " +
+           " WHERE report = ?1 " + 
+           "GROUP BY " +
+           "    f.hash, f.size")
+    List<Duplicate> findDuplicates(Report report);
+}
