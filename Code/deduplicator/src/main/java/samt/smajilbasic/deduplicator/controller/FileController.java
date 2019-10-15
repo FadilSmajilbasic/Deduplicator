e49e14cfda4d7b2ede9ed8d4cd142b62c025355e
Fadil Smajilbasic <fadil.smajilbasic@samtrevano.ch>
2019-10-15 16:19:37 +0200

    update FileController

--- Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/controller/FileController.java
+++ Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/controller/FileController.java
@@ -14,12 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import samt.smajilbasic.deduplicator.Validator;
 import samt.smajilbasic.deduplicator.entity.File;
 import samt.smajilbasic.deduplicator.exception.ErrorMessage;
 import samt.smajilbasic.deduplicator.repository.FileRepository;
-
-
-
+import samt.smajilbasic.deduplicator.repository.ReportRepository;
 
 @RestController
 @RequestMapping(path = "/file")
@@ -28,17 +27,23 @@ public class FileController {
     @Autowired
     FileRepository fileRepository;
 
+    @Autowired
+    ReportRepository reportRepository;
+
     @GetMapping(value = "/get")
     public @ResponseBody Iterable<File> getFiles() {
         return fileRepository.findAll();
     }
 
-    @GetMapping(value = "/get/{path}")
-    public @ResponseBody Object getFileByPath(@PathVariable String path) {
-        if(path != null && fileRepository.existsById(path))
-            return fileRepository.findById(path).get();
+    @GetMapping(value = "/get/{id}")
+    public @ResponseBody Object getFilesFromReport(@PathVariable String id) {
+        Integer intId = Validator.isInt(id);
+        if (intId != null && reportRepository.existsById(intId))
+            return reportRepository.findById(intId).get().getFile();
         else
-            return new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR,"Invalid file path");
+            return new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, "Invalid report id");
     }
+
     
+
 }
