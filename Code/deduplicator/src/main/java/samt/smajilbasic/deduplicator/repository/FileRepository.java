f001135bf8786fdd21b8987704c5812f1f19308b
Fadil Smajilbasic <fadil.smajilbasic@samtrevano.ch>
2019-10-15 16:26:51 +0200

    update FileRepository

--- Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/repository/FileRepository.java
+++ Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/repository/FileRepository.java
@@ -1,6 +1,11 @@
 package samt.smajilbasic.deduplicator.repository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import samt.smajilbasic.deduplicator.entity.Report;
 import samt.smajilbasic.deduplicator.entity.File;
 
 /**
@@ -9,5 +14,11 @@ import samt.smajilbasic.deduplicator.entity.File;
 @Repository
 public interface FileRepository extends CrudRepository<File,String>{
 
-    
+    @Query( value =  "SELECT * FROM file f where f.report=?1 and f.hash = ?2",nativeQuery = true)
+    List<File> findFilesFromHashAndReport(Report report,String hash);
+
+
+
+    @Query(value = "SELECT if(EXISTS(SELECT * from file WHERE hash=?1)>0,'true','false')",nativeQuery = true)
+    boolean existsByHash(String hash);
 }
