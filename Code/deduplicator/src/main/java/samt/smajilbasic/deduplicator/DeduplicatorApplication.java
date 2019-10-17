42280751699b886e50053afaa38aded92afc327e
Fadil Smajilbasic <fadil.smajilbasic@samtrevano.ch>
2019-10-17 16:21:17 +0200

    Added MYSQL setting for timezone on app start

--- Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/DeduplicatorApplication.java
+++ Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/DeduplicatorApplication.java
@@ -1,13 +1,23 @@
 package samt.smajilbasic.deduplicator;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 // (scanBasePackages = { "samt.smajilbasic.deduplicator.controller", "samt.smajilbasic.deduplicator.repository","samt.smajilbasic.deduplicator.entity"})
 @SpringBootApplication
 public class DeduplicatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeduplicatorApplication.class, args);
+	}
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getDefault());
 	}
 
 }
