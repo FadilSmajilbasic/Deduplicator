3e2f968c098c045540022624e50d8b8e903d968a
Fadil Smajilbasic <fadil.smajilbasic@samtrevano.ch>
2019-11-05 17:58:00 +0100

    update: ActionController completed executeActions function

--- Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/controller/ActionController.java
+++ Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/controller/ActionController.java
@@ -2,6 +2,8 @@ package samt.smajilbasic.deduplicator.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
  * ScanController
@@ -20,8 +22,11 @@ import samt.smajilbasic.deduplicator.ActionType;
 import samt.smajilbasic.deduplicator.PathType;
 import samt.smajilbasic.deduplicator.Validator;
 import samt.smajilbasic.deduplicator.entity.Action;
+import samt.smajilbasic.deduplicator.entity.AuthenticationDetails;
 import samt.smajilbasic.deduplicator.exception.Message;
 import samt.smajilbasic.deduplicator.repository.ActionRepository;
+import samt.smajilbasic.deduplicator.repository.AuthenticationDetailsRepository;
+import samt.smajilbasic.deduplicator.worker.ActionsManager;
 
 @RestController
 @RequestMapping(path = "/action")
@@ -29,6 +34,9 @@ public class ActionController {
 
     @Autowired
     ActionRepository actionRepository;
+
+    @Autowired
+    AuthenticationDetailsRepository adr;
 
     @GetMapping()
     public @ResponseBody Iterable<Action> getFiles() {
@@ -45,47 +53,52 @@ public class ActionController {
     }
 
     @PostMapping("/execute/")
-    public @ResponseBody Message executeActions(){
+    public @ResponseBody Message executeActions() {
 
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String authenticatedUser = authentication.getName();
+
+        AuthenticationDetails internalUser = adr.findById(authenticatedUser).get();
+
+        new ActionsManager(actionRepository.findActionsFromUser(internalUser), internalUser);
         return new Message(HttpStatus.OK);
     }
 
     @PutMapping()
-    public @ResponseBody Object addAction(@RequestParam String type, @RequestParam String path,@RequestParam String newPath ){
-        
+    public @ResponseBody Object addAction(@RequestParam String type, @RequestParam String path,
+            @RequestParam String newPath, @RequestParam(required = false) String user) {
 
-        type = getType(type);
-        if (type == null) 
-            return new Message(HttpStatus.INTERNAL_SERVER_ERROR,"Action type invalid");
-        
-        if(type.equals(ActionType.MOVE) && (newPath.trim().equals("") || newPath == null))
-            return new Message(HttpStatus.INTERNAL_SERVER_ERROR,"New path not set while having type = MOVE");
-        
-        if(Validator.getPathType(path) != PathType.File)
-            return new Message(HttpStatus.INTERNAL_SERVER_ERROR,"File path invalid");
+        if (adr.existsById(user)) {
+            AuthenticationDetails internalUser = adr.findById(user).get();
+            type = getType(type);
+            if (type == null)
+                return new Message(HttpStatus.INTERNAL_SERVER_ERROR, "Action type invalid");
 
-        if(Validator.getPathType(newPath) != PathType.Directory )
-            return new Message(HttpStatus.INTERNAL_SERVER_ERROR,"New path is invalid or not a directory");
+            if (type.equals(ActionType.MOVE) && (newPath.trim().equals("") || newPath == null))
+                return new Message(HttpStatus.INTERNAL_SERVER_ERROR, "New path not set while having type = MOVE");
 
+            if (Validator.getPathType(path) != PathType.File)
+                return new Message(HttpStatus.INTERNAL_SERVER_ERROR, "File path invalid");
 
-        Action action = new Action(type, path, newPath);
-        actionRepository.save(action);
+            if (Validator.getPathType(newPath) != PathType.Directory)
+                return new Message(HttpStatus.INTERNAL_SERVER_ERROR, "New path is invalid or not a directory");
 
-        return action;
+            Action action = new Action(type, path, newPath, internalUser);
+            actionRepository.save(action);
+
+            return action;
+        } else {
+            return new Message(HttpStatus.INTERNAL_SERVER_ERROR, "User invalid");
+        }
     }
 
-    private String getType(String type){
-        if(type.equalsIgnoreCase(ActionType.DELETE) ||
-            type.equalsIgnoreCase(ActionType.MOVE) ||
-            type.equalsIgnoreCase(ActionType.DELETE) ||
-            type.equalsIgnoreCase(ActionType.IGNORE) ||
-            type.equalsIgnoreCase(ActionType.NONE))
+    private String getType(String type) {
+        if (type.equalsIgnoreCase(ActionType.DELETE) || type.equalsIgnoreCase(ActionType.MOVE)
+                || type.equalsIgnoreCase(ActionType.DELETE) || type.equalsIgnoreCase(ActionType.IGNORE)
+                || type.equalsIgnoreCase(ActionType.NONE))
             return type.toUpperCase();
         else
             return null;
     }
-
-
-    
 
 }
