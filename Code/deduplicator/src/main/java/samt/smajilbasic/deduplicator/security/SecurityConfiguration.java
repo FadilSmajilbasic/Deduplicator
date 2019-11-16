95046d81da32818d4c02b3d9a65e40726d8c342a
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2019-11-16 15:08:24 +0100

    update SecurityConfiguration

--- Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/security/SecurityConfiguration.java
+++ Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/security/SecurityConfiguration.java
@@ -3,14 +3,19 @@ package samt.smajilbasic.deduplicator.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer.UserDetailsBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import samt.smajilbasic.deduplicator.repository.AuthenticationDetailsRepository;
 
@@ -27,25 +32,48 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 
-        adr.findAll().forEach(user -> {
-            String type = "USER";
-            if (user.getUsername().equals("admin")) {
+
+        PasswordEncoder encoder = passwordEncoder();
+
+        adr.findAll().forEach(user -> { // get every user form the database
+            String type = "USER"; // set type of user
+            if (user.getUsername().equals("admin")) { // check if they are admin
                 type = "ADMIN";
             }
 
             try {
-                auth.inMemoryAuthentication().withUser(user.getUsername()).password(user.getPassword())
-                        .roles(type);
+                auth.inMemoryAuthentication().passwordEncoder(encoder)
+                        .withUser(user.getUsername()) // add user to the authentication memory
+                        .password(encoder.encode(user.getPassword())) // add user's password 
+                        .roles(type); // set user type
+
+                System.out.println("user: " + user.getUsername() + " pass: " + user.getPassword());
+
             } catch (Exception e) {
                 System.out.println("[ERROR] Unable to add user for http authentication: " + user.getUsername());
                 e.printStackTrace();
             }
+
         }
 
         );
 
+        try {
+            System.out.println("Stored: " + auth.inMemoryAuthentication().getUserDetailsService().userExists("admin")); // user not found
+        } catch (UsernameNotFoundException e) {
+            System.out.println("[ERROR] UsernameNotFoundException " );
+
+        } catch (Exception e) {
+            System.out.println("[ERROR] Exception: " );
+
+        }
+
     }
 
+    /**
+     * Used to encode the user password, currently unused because I'm testing with plain text passwords
+     * If setup as bean, Spring will detect the password encryption type automatically
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -55,16 +83,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                // HTTP Basic authentication
-                .httpBasic()
-                .and()
-                .authorizeRequests().anyRequest().hasAnyRole("USER","ADMIN")
+                .authorizeRequests().anyRequest().authenticated() // check if request is authorized on any request, any type of use can access any request 
                 .and()
                 .csrf().disable()
-                .formLogin().disable()
-                .logout()
+                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint) // on exception use authenticationEntryPoint to make the response
                 .and()
-                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
+                .httpBasic(); // HTTP Basic authentication
 
     }
 }
