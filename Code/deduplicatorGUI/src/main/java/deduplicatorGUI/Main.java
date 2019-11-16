aa2511058434ff88ebed1bc4aaf171bb8d8bc69c
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2019-11-16 15:11:05 +0100

    Update main class

--- Code/deduplicatorGUI/src/main/java/deduplicatorGUI/Main.java
+++ Code/deduplicatorGUI/src/main/java/deduplicatorGUI/Main.java
@@ -5,9 +5,11 @@
  */
 package deduplicatorGUI;
 
+import deduplicatorGUI.layouts.MainJFrame;
+
 /**
  *
- * @author duck
+ * @author Fadil Smajilbasic
  */
 public class Main {
 
@@ -15,7 +17,11 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainJFrame().setVisible(true);
+            }
+        });
     }
     
 }
