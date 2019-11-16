4e010c2f25ffbda63665e549b59a89e6ad1187f7
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2019-11-16 15:10:55 +0100

    Added login controller

---
+++ Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/controller/LoginController.java
@@ -0,0 +1,20 @@
+package samt.smajilbasic.deduplicator.controller;
+
+import  samt.smajilbasic.deduplicator.exception.Message;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/login")
+public class LoginController {
+
+    @GetMapping("/")
+    public @ResponseBody Message checkLogin(){
+        return new Message(HttpStatus.OK, "User authenticated sucessfuly");
+    }
+
+}
