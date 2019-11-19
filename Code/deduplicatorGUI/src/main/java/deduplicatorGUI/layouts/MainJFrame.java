80caa1e687665cac53b769137fe2630e0dfc0614
duck <duck@pop-os.localdomain>
2019-11-19 16:07:45 +0100

    Update MainJFrame

--- Code/deduplicatorGUI/src/main/java/deduplicatorGUI/layouts/MainJFrame.java
+++ Code/deduplicatorGUI/src/main/java/deduplicatorGUI/layouts/MainJFrame.java
@@ -1,11 +1,24 @@
 
 package deduplicatorGUI.layouts;
 
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import deduplicatorGUI.communication.Client;
+import deduplicatorGUI.listeners.UserConnectedListener;
+
 /**
  *
- * @author duck
+ * @author Fadil Smajilbasic
  */
-public class MainJFrame extends javax.swing.JFrame {
+public class MainJFrame extends JFrame implements UserConnectedListener, ChangeListener {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
     /**
      * Creates new form MainJFrame
@@ -20,35 +33,41 @@ public class MainJFrame extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-
         connectionPanel = new ConnectionJPanel();
         duplicatePanel = new DuplicateJPanel();
         scanPanel = new ScanJPanel();
         pathPanel = new PathJPanel();
         schedulePanel = new ScheduleJPanel();
-        menu.add("Connection",connectionPanel);
-        menu.add("Duplicates",duplicatePanel);
-        menu.add("Scan",scanPanel);
-        menu.add("Path",pathPanel);
-        menu.add("Schedule",schedulePanel);
 
+        connectionPanel.setListener(this);
 
+        menu.add("Connection", connectionPanel);
+        menu.add("Duplicates", duplicatePanel);
+        menu.add("Scan", scanPanel);
+        menu.add("Path", pathPanel);
+        menu.add("Schedule", schedulePanel);
+
+        menu.addChangeListener(this);
+
+        changeTabsAccessibility(menu, false);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(menu, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(menu, javax.swing.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
-        );
-
-
-
+        layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(menu, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE));
+        layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addComponent(menu,
+                javax.swing.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE));
 
         pack();
+    }
+
+    private void changeTabsAccessibility(JTabbedPane menu, boolean state) {
+        for (int i = 1; i < menu.getTabCount(); i++) {
+            menu.setEnabledAt(i, state);
+            if (state)
+                ((BaseJPanel) menu.getComponentAt(i)).setClient(client);
+
+        }
     }
 
     private ConnectionJPanel connectionPanel;
@@ -58,4 +77,17 @@ public class MainJFrame extends javax.swing.JFrame {
     private ScheduleJPanel schedulePanel;
 
     private javax.swing.JTabbedPane menu;
+    private Client client;
+
+    @Override
+    public void userConnected(Client client) {
+        System.out.println("frame");
+        changeTabsAccessibility(menu, true);
+        this.client = client;
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+       ( (BaseJPanel)menu.getSelectedComponent()).tabSelected();
+    }
 }
