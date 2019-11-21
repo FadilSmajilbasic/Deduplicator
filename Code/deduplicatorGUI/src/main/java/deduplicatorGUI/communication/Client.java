57a903f92e8f9e2405c8670462b378dc8c8b3190
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2019-11-21 16:22:44 +0100

    Update client

--- Code/deduplicatorGUI/src/main/java/deduplicatorGUI/communication/Client.java
+++ Code/deduplicatorGUI/src/main/java/deduplicatorGUI/communication/Client.java
@@ -4,6 +4,14 @@ package deduplicatorGUI.communication;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.HttpRequestHandlerAdapter;
 
 import java.io.IOException;
 import java.net.Authenticator;
@@ -15,6 +23,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Map;
+import java.util.function.BiConsumer;
 
 /**
  *
@@ -27,8 +36,23 @@ public class Client {
     private String username;
 
     private String password;
-    private String ip;
+    private InetAddress addr;
+
     private int port;
+    private URI uri;
+
+    private final JSONParser parser = new JSONParser();
+
+    /**
+     * @param port the port to set
+     */
+    public void setPort(int port) {
+        if (port > 0 && port < 65535) {
+            this.port = port;
+        } else {
+            port = 8080;
+        }
+    }
 
     public Client(String username, String password) {
 
@@ -57,8 +81,9 @@ public class Client {
             JSONObject jsonObj = (JSONObject) responseObj;
             String stat = (String) jsonObj.get("status");
 
-            System.out.println("stat: " + stat);
-            return (stat.equals("OK"));
+            this.addr = addr;
+            setPort(port);
+            return stat.equals("OK");
 
         } catch (URISyntaxException ex) {
             System.out.println("Uri exception " + ex.getMessage());
@@ -73,22 +98,17 @@ public class Client {
 
     }
 
-    public JSONObject getAll(String path) {
+    public Object getAll(String path) {
         try {
 
-            var request = HttpRequest.newBuilder().uri(new URI("http://localhost:8080/" + path)).GET().build();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("http://" + addr.getHostAddress() + ":" + port + "/" + path)).GET().build();
 
             HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
             Object responseObj = new JSONParser().parse(response.body());
 
-            JSONObject jsonObj = (JSONObject) responseObj;
-
-            String stat = (String) jsonObj.get("status");
-            if (stat.equals("OK")) {
-                System.out.println("stat is 200");
-                return jsonObj;
-            }
+            return responseObj;
 
         } catch (URISyntaxException ex) {
             System.out.println("deduplicatorGUI.communication.Client.getAll()");
@@ -98,7 +118,7 @@ public class Client {
         } catch (IOException ioe) {
             System.err.println("ioe" + ioe.getMessage());
         } catch (ParseException pe) {
-            System.err.println("parse" + pe.getMessage());
+            System.err.println("parse get : " + pe.getStackTrace());
 
         }
         return null;
@@ -106,6 +126,50 @@ public class Client {
 
     public Object get(String path) {
         return null;
+    }
+
+    public Object delete(String path, String param) {
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("path", param);
+
+        RestTemplate restTemplate = new RestTemplate();
+        // HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+        // ResponseEntity<String> response = restTemplate.postForEntity("http://" +
+        // addr.getHostAddress() + ":" + port + "/form", request, String.class);
+
+        restTemplate.delete("http://" + addr.getHostAddress() + ":" + port + "/path/" + param.replace("/", "&47#;"),
+                map);
+        return new Object();
+        // try {
+
+        // HttpRequest request = HttpRequest.newBuilder()
+        // .uri(new URI("http://" + addr.getHostAddress() + ":" + port + "/" + path))
+        // .method("DELETE", HttpRequest.BodyPublishers.ofString("path:" +
+        // param)).build();
+
+        // HttpResponse<String> response = httpClient.send(request,
+        // HttpResponse.BodyHandlers.ofString());
+
+        // System.out.println("body: " + response.body());
+        // Object resp = parser.parse(response.body());
+
+        // return resp;
+
+        // } catch (URISyntaxException ex) {
+        // System.out.println("deduplicatorGUI.communication.Client.delete()");
+        // } catch (InterruptedException ie) {
+        // System.out.println("interr delete: " + ie.getMessage());
+        // } catch (IOException ioe) {
+        // System.err.println("ioe delete: " + ioe.getMessage());
+        // } catch (ParseException pe) {
+        // System.err.println("parse ex delete :" + pe.getMessage());
+        // }
+
+        // return null;
     }
 
     public Object post(String path, Map<String, String> values) {
