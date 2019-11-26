56beb316a3d00d643e894f6e04eb72704db58cec
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2019-11-26 16:54:05 +0100

    update  BaseJPanel

--- Code/deduplicatorGUI/src/main/java/deduplicatorGUI/layouts/BaseJPanel.java
+++ Code/deduplicatorGUI/src/main/java/deduplicatorGUI/layouts/BaseJPanel.java
@@ -2,6 +2,11 @@ package deduplicatorGUI.layouts;
 
 import javax.swing.JPanel;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import deduplicatorGUI.communication.Client;
 
 /**
@@ -30,6 +35,22 @@ public class BaseJPanel extends JPanel{
      */
     public void tabSelected(){
 
+    }
+
+
+    public JSONObject[] getArray(JSONArray array) {
+
+        Object[] objectArray = (Object[]) array.toArray();
+        JSONParser parser = new JSONParser();
+        JSONObject[] result = new JSONObject[objectArray.length];
+        for (int i = 0; i < objectArray.length; i++) {
+            try {
+                result[i] = (JSONObject) parser.parse(objectArray[i].toString());
+            } catch (ParseException e) {
+                System.out.println("unable to parse " + objectArray[i].toString());
+            }
+        }
+        return result;
     }
     
 }
