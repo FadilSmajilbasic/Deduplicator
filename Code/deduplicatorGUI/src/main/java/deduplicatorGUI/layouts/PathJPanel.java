6e670c3c1a280626e280fe918dc9cd033d1d5a08
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2019-11-29 17:12:07 +0100

    Update PathJPanel

--- Code/deduplicatorGUI/src/main/java/deduplicatorGUI/layouts/PathJPanel.java
+++ Code/deduplicatorGUI/src/main/java/deduplicatorGUI/layouts/PathJPanel.java
@@ -4,6 +4,7 @@ package deduplicatorGUI.layouts;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractListModel;
+import javax.swing.BorderFactory;
 import javax.swing.JOptionPane;
 
 import org.json.simple.JSONArray;
@@ -18,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 public class PathJPanel extends BaseJPanel {
 
     private static final long serialVersionUID = 2506225118625343558L;
+    private static final int MARGIN = 20;
 
     /**
      * Creates new form PathJPanel
@@ -28,18 +30,23 @@ public class PathJPanel extends BaseJPanel {
 
     private void initComponents() {
 
+        java.awt.GridBagConstraints gridBagConstraints;
+
         pathTextField = new javax.swing.JTextField();
         typeComboBox = new javax.swing.JComboBox<>();
-        insertButton = new javax.swing.JButton();
-        deleteButton = new javax.swing.JButton();
         pathJScrollPane = new javax.swing.JScrollPane();
         pathJList = new javax.swing.JList<>();
+        typeJScrollPane = new javax.swing.JScrollPane();
+        typeJList = new javax.swing.JList<>();
+        insertButton = new javax.swing.JButton();
+        deleteButton = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
-        pathTextField.setText("path");
-
-        typeComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Scan", "Ignore" }));
-
-        insertButton.setText("Insert");
+        java.awt.GridBagLayout layout = new java.awt.GridBagLayout();
+        layout.columnWidths = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        layout.rowHeights = new int[] { 0, 10, 0, 10, 0, 10, 0, 10, 0 };
+        setLayout(layout);
 
         insertButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -48,47 +55,73 @@ public class PathJPanel extends BaseJPanel {
 
         });
 
-        deleteButton.setText("Delete");
         deleteButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 deleteButtonActionPerformed(evt);
             }
         });
 
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipadx = 350;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_START;
+        add(pathTextField, gridBagConstraints);
+
+        typeComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Scan", "Ignore" }));
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 4;
+        gridBagConstraints.gridy = 0;
+        add(typeComboBox, gridBagConstraints);
+
         pathJScrollPane.setViewportView(pathJList);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout
-                .createSequentialGroup().addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addGroup(layout.createSequentialGroup()
-                                .addComponent(pathTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 163,
-                                        javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18).addComponent(typeComboBox, javax.swing.GroupLayout.PREFERRED_SIZE,
-                                        javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addComponent(pathJScrollPane))
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup().addGap(18, 18, 18).addComponent(insertButton).addGap(0,
-                                0, Short.MAX_VALUE))
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING,
-                                layout.createSequentialGroup().addGap(39, 39, 39).addComponent(deleteButton)))
-                .addContainerGap()));
-        layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(layout.createSequentialGroup().addContainerGap()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                .addComponent(pathTextField, javax.swing.GroupLayout.PREFERRED_SIZE,
-                                        javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addComponent(typeComboBox, javax.swing.GroupLayout.PREFERRED_SIZE,
-                                        javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addComponent(insertButton))
-                        .addGap(18, 18, 18)
-                        .addComponent(pathJScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 188,
-                                javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED,
-                                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(deleteButton).addContainerGap()));
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        add(pathJScrollPane, gridBagConstraints);
 
+        typeJScrollPane.setViewportView(typeJList);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 4;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        add(typeJScrollPane, gridBagConstraints);
+
+        deleteButton.setText("Delete");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 8;
+        gridBagConstraints.ipady = 20;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_START;
+        add(deleteButton, gridBagConstraints);
+
+        insertButton.setText("Insert");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 8;
+        gridBagConstraints.gridy = 0;
+        add(insertButton, gridBagConstraints);
+
+        jLabel1.setText("Path");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_START;
+        add(jLabel1, gridBagConstraints);
+
+        jLabel2.setText("Ignore");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 4;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_START;
+        add(jLabel2, gridBagConstraints);
+
+        this.setBorder(BorderFactory.createEmptyBorder(MARGIN, MARGIN, MARGIN, MARGIN));
     }
 
     private void deleteButtonActionPerformed(java.awt.event.ActionEvent evt) {
@@ -147,6 +180,16 @@ public class PathJPanel extends BaseJPanel {
                 }
             });
 
+            typeJList.setModel(new AbstractListModel<String>() {
+                public int getSize() {
+                    return array.length;
+                }
+
+                public String getElementAt(int i) {
+                    return array[i].get("ignoreFile").toString() ;
+                }
+            });
+
         } else {
             JOptionPane.showMessageDialog(this, "Unable to get retrieve paths", "Get error ",
                     JOptionPane.INFORMATION_MESSAGE);
@@ -160,7 +203,11 @@ public class PathJPanel extends BaseJPanel {
     private javax.swing.JButton deleteButton;
     private javax.swing.JButton insertButton;
     private javax.swing.JList<String> pathJList;
+    private javax.swing.JList<String> typeJList;
     private javax.swing.JScrollPane pathJScrollPane;
+    private javax.swing.JScrollPane typeJScrollPane;
     private javax.swing.JComboBox<String> typeComboBox;
 
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
 }
