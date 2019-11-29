747b4744f62b8f85c480fd1642659a6977f8bcb7
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2019-11-29 17:09:28 +0100

    Update Hasher

--- Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/scanner/Hasher.java
+++ Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/scanner/Hasher.java
@@ -1,17 +1,11 @@
 package samt.smajilbasic.deduplicator.scanner;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.LinkedList;
-import java.util.Scanner;
 
 import samt.smajilbasic.deduplicator.entity.Report;
 import samt.smajilbasic.deduplicator.entity.File;
@@ -22,45 +16,50 @@ import samt.smajilbasic.deduplicator.repository.FileRepository;
  */
 public class Hasher extends Thread {
 
-    private LinkedList<java.io.File> files;
+    private java.io.File file;
     ScannerThreadListener stl;
     Report report;
 
     private FileRepository fileRepository;
 
-    public Hasher(LinkedList<java.io.File> files, Report report, ScannerThreadListener stl,
-            FileRepository fileRepository) {
-        this.files = files;
+    Object monitor;
+
+    boolean paused;
+
+    public Hasher(java.io.File file, Report report, ScannerThreadListener stl, FileRepository fileRepository,
+            Object monitor) {
+        this.file = file;
         this.report = report;
         this.stl = stl;
         this.fileRepository = fileRepository;
+        this.monitor = monitor;
     }
 
-    public static String getHash(RandomAccessFile file, String mode) throws NoSuchAlgorithmException {
+    public String getHash(RandomAccessFile file, String mode) throws NoSuchAlgorithmException {
 
         MessageDigest messageDigest = MessageDigest.getInstance(mode);
 
-        int buff = 16384;
+        int buff = 32768;
         try {
-
+            
             byte[] buffer = new byte[buff];
-
             long read = 0;
 
             long end = file.length();
             int unitsize;
             long start = System.currentTimeMillis();
             while (read < end) {
+                checkPaused();
                 unitsize = (int) (((end - read) >= buff) ? buff : (end - read));
                 file.read(buffer, 0, unitsize);
                 messageDigest.update(buffer, 0, unitsize);
                 read += unitsize;
             }
-            System.out.println("Hash duration: "+(System.currentTimeMillis()-start) + "ms");
+            System.out.println("Hash duration: " + (System.currentTimeMillis() - start) + "ms");
 
         } catch (FileNotFoundException fnfE) {
             System.out.println("[ERROR] Hasher: Item not found");
-        
+
         } catch (IOException ioE) {
             System.out.println("[ERROR] Hasher: IO Exception");
         }
@@ -79,32 +78,59 @@ public class Hasher extends Thread {
     @Override
     public void run() {
 
-        while (files.peek() != null) {
-            java.io.File file = files.poll();
-            Long lastModified = file.lastModified();
+        Long lastModified = file.lastModified();
 
-            try {
-                RandomAccessFile fileRAF = new RandomAccessFile(file.getAbsolutePath(), "r");
-                String hash = Hasher.getHash(fileRAF, "MD5");
-                long size = fileRAF.length();
+        try {
+            RandomAccessFile fileRAF = new RandomAccessFile(file.getAbsolutePath(), "r");
+            String hash = getHash(fileRAF, "MD5");
+            long size = fileRAF.length();
 
-                fileRAF.close();
+            fileRAF.close();
 
-                File record = new File(file.getAbsolutePath(), lastModified, hash, size, report);
-                fileRepository.save(record);
-                stl.addFilesScanned();
+            File record = new File(file.getAbsolutePath(), lastModified, hash, size, report);
+            fileRepository.save(record);
+            stl.addFilesScanned();
 
-            } catch (NoSuchAlgorithmException nsae) {
-                System.err.println("[ERROR] Unable to hash file: " + nsae.getMessage());
+        } catch (NoSuchAlgorithmException nsae) {
+            System.err.println("[ERROR] Unable to hash file: " + nsae.getMessage());
 
-            } catch (IOException ioe) {
-                System.err.println("[ERROR] Unable to read file: " + ioe.getMessage());
-            } catch (NullPointerException npe) {
-                System.err.println("[ERROR] Unable to save file: " + npe.getMessage());
-            } catch (OutOfMemoryError ex) {
-                System.out.println("[ERROR] File too big to calculate hash: " + file.getAbsolutePath());
-                System.out.println(ex.getMessage());
+        } catch (IOException ioe) {
+            System.err.println("[ERROR] Unable to read file: " + ioe.getMessage());
+        } catch (NullPointerException npe) {
+            System.err.println("[ERROR] Unable to save file: " + npe.getMessage());
+        } catch (OutOfMemoryError ex) {
+            System.out.println("[ERROR] File too big to calculate hash: " + file.getAbsolutePath());
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public synchronized void checkPaused() {
+        synchronized (monitor) {
+            while (isPaused()) {
+                try {
+                    monitor.wait();
+                    System.out.println("[INFO] Threads resumed");
+                } catch (InterruptedException e) {
+                    System.out.println("[INFO] Interrupted exception on pause: " + e.getStackTrace().toString());
+                }
+                System.out.println("wait ended");
             }
         }
+
+    }
+
+    public void pause() {
+        if (isAlive()) {
+            this.paused = true;
+            System.out.println("[INFO] Paused hasher ");
+        }
+
+    }
+
+    /**
+     * @return the paused
+     */
+    public boolean isPaused() {
+        return paused;
     }
 }
