bca9a6c63774f12aae3e76d2bf53c6863d682a8d
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2019-12-16 00:22:24 +0100

    Update ReportRepository

--- Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/repository/ReportRepository.java
+++ Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/repository/ReportRepository.java
@@ -1,5 +1,7 @@
 package samt.smajilbasic.deduplicator.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -14,5 +16,8 @@ public interface ReportRepository extends CrudRepository<Report, Integer> {
 
     @Query(value = "SELECT * FROM report ORDER BY report_id DESC limit 1;",nativeQuery = true)
     public Report getLastReport();
+
+    @Query(value="SELECT report_id, start FROM report;",nativeQuery = true)
+    public List<String> findAllReduced();
 
 }
