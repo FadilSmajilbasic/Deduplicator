f4cd9c9de6c7f5eec8750971b6b584d74d4d6085
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2020-02-14 16:29:40 +0100

    update main view

--- Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/views/MainView.java
+++ Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/views/MainView.java
@@ -4,11 +4,8 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.navigator.View;
-
-import samt.smajilbasic.communication.Client;
-import com.vaadin.flow.component.contextmenu.MenuItem;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.menubar.MenuBar;
 
 /**
  * MainView
@@ -19,15 +16,7 @@ import com.vaadin.flow.component.menubar.MenuBar;
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
 public class MainView extends VerticalLayout implements View{
 
-        private MenuBar menuBar;
-        private MenuItem path;
-        private MenuItem login;
-
         public MainView() {
-                menuBar = new MenuBar();
-                login = menuBar.addItem("Login", e -> login.getUI().ifPresent(ui -> ui.navigate("login")));
-                path = menuBar.addItem("Path", e -> path.getUI().ifPresent(ui -> ui.navigate("path")));
-                menuBar.setSizeFull();
-                add(menuBar);
+                UI.getCurrent().getPage().setLocation("login/");
         }
 }
