9788b58ca24ad203542c92172cdf2afdf9a066bf
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2020-02-19 16:26:21 +0100

    LoginView: Added new error messages and added commented

--- Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/views/LoginView.java
+++ Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/views/LoginView.java
@@ -1,13 +1,17 @@
 package samt.smajilbasic.views;
 
+import com.vaadin.data.Binder;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.orderedlayout.FlexLayout.WrapMode;
 import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
@@ -16,31 +20,61 @@ import com.vaadin.navigator.View;
 
 import org.springframework.http.HttpStatus;
 
+import samt.smajilbasic.Validator;
 import samt.smajilbasic.communication.Client;
 
+/**
+ * The Login view that the user uses to authenticate to the deduplicator
+ * service.
+ */
 @Route(value = "login", registerAtStartup = true)
 public class LoginView extends VerticalLayout implements View {
 
     private static final long serialVersionUID = 4944489863331319773L;
 
+    /**
+     * The HTTP/HTTPS client uset for the authentication.
+     */
     private Client client;
+
+    /**
+     * Token used to identify the {@link Client} when passing it between classes.
+     */
     public final static String CLIENT_STRING = "client";
 
+    /**
+     * Flag used to switch between the advanced and basic view.
+     */
     private boolean defaultView = true;
+    /**
+     * The text field for the host IP.
+     */
     private TextField hostTextField;
+    /**
+     * The text field for the host port.
+     */
     private NumberField portTextField;
+    /**
+     * The button that switches between the views.
+     */
+    private Button advancedViewButton;
 
+    /**
+     * Base constructor of the LoginView class. It creates the LoginView GUI with
+     * all of it's elements.
+     */
     public LoginView() {
 
-        HorizontalLayout horizontalLayout = new HorizontalLayout();
-        HorizontalLayout credentialsLayout = new HorizontalLayout();
+        
 
         hostTextField = new TextField("Host");
         portTextField = new NumberField("Port");
         TextField usernameTextField = new TextField("Username");
         PasswordField passwordField = new PasswordField("Password");
 
-        Button advancedViewButton = new Button("Advanced View",event ->{toggleView();});
+        advancedViewButton = new Button("Advanced View", event -> {
+            toggleView();
+        });
         advancedViewButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
 
         portTextField.setStep(1);
@@ -58,44 +92,70 @@ public class LoginView extends VerticalLayout implements View {
 
         button.addClickShortcut(Key.ENTER);
 
-        horizontalLayout.add(hostTextField, portTextField);
-        credentialsLayout.add(usernameTextField, passwordField);
+        HorizontalLayout horizontalLayout = new HorizontalLayout(hostTextField, portTextField);
+        HorizontalLayout credentialsLayout = new HorizontalLayout(usernameTextField, passwordField);
         portTextField.setWidthFull();
 
         this.setAlignItems(Alignment.CENTER);
+        this.setFlexGrow(1, this);
         toggleView();
 
-        add(horizontalLayout, credentialsLayout, button,advancedViewButton);
+        add(horizontalLayout, credentialsLayout, button, advancedViewButton);
 
     }
 
+    /**
+     * The toggleView method switches between the advanced and regular login view.
+     * By switching to the advanced view it exposes the host and port fields.
+     */
     private void toggleView() {
-        if (defaultView) {
-            hostTextField.setVisible(!defaultView);
-            portTextField.setVisible(!defaultView);
-        } else {
-            hostTextField.setVisible(!defaultView);
-            portTextField.setVisible(!defaultView);
-        }
+        hostTextField.setVisible(!defaultView);
+        portTextField.setVisible(!defaultView);
+        advancedViewButton.setText(defaultView ? "Advanced View" : "Basic View");
         defaultView = !defaultView;
     }
 
-    public void tryLogin(String host, int port, String user, String pass) {
+    /**
+     * The tyLogin method attempts to authenticate the {@link Client} to the
+     * deduplicator service.
+     * 
+     * @param host the host IP to conenct to.
+     * @param port the port to connect to.
+     * @param user the username to try to authenticate with.
+     * @param pass the user's password to try to authenticate with.
+     */
+    private void tryLogin(String host, int port, String user, String pass) {
 
-        client = new Client(user, pass);
-        HttpStatus resp = client.isAuthenticated(host, port);
+        if (Validator.isValidIP(host)) {
+            if (port > 0 && port <= 65535) {
+                client = new Client(user, pass);
+                HttpStatus resp = client.isAuthenticated(host, port);
 
-        switch (resp) {
-        case OK:
-            UI.getCurrent().getSession().setAttribute(CLIENT_STRING, client);
-            UI.getCurrent().navigate("path");
-            break;
-        case SERVICE_UNAVAILABLE:
-            Notification.show("Server not reachable or an error occured", 2000, Notification.Position.TOP_END).addThemeVariants(NotificationVariant.LUMO_ERROR);
-            break;
-        case UNAUTHORIZED:
-            Notification.show("Invalid credentials", 2000, Notification.Position.TOP_END).addThemeVariants(NotificationVariant.LUMO_ERROR);
-            break;
+                switch (resp) {
+                case OK:
+                    UI.getCurrent().getSession().setAttribute(CLIENT_STRING, client);
+                    UI.getCurrent().navigate("path");
+                    break;
+                case SERVICE_UNAVAILABLE:
+                    Notification.show("Server not reachable or an error occured", 2000, Notification.Position.TOP_END)
+                            .addThemeVariants(NotificationVariant.LUMO_ERROR);
+                    break;
+                case UNAUTHORIZED:
+                    Notification.show("Invalid credentials", 2000, Notification.Position.TOP_END)
+                            .addThemeVariants(NotificationVariant.LUMO_ERROR);
+                    break;
+                default:
+                    Notification.show("Unknown error occured", 2000, Notification.Position.TOP_END)
+                            .addThemeVariants(NotificationVariant.LUMO_ERROR);
+                    break;
+                }
+            } else {
+                Notification.show("Invalid port set", 2000, Notification.Position.TOP_END)
+                        .addThemeVariants(NotificationVariant.LUMO_ERROR);
+            }
+        } else {
+            Notification.show("Invalid IP set", 2000, Notification.Position.TOP_END)
+                    .addThemeVariants(NotificationVariant.LUMO_ERROR);
         }
     }
 
