484d61c7fc33561ee2aa528d15eac606410879bf
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2020-03-05 16:51:33 +0100

    Added ActionTyoe from deduplicator

---
+++ Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/ActionType.java
@@ -0,0 +1,8 @@
+package samt.smajilbasic;
+
+public interface ActionType {
+    public static String  DELETE = "DELETE";
+    public static String  MOVE = "MOVE";
+    public static String  IGNORE = "IGNORE";
+    public static String  SCAN = "SCAN";
+}
