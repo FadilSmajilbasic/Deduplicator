6aa4fe2b217871b19bec1a01a758a651e9712e1e
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2020-03-06 16:15:42 +0100

    SchedulerView: added date and time functionality

--- Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/views/SchedulerView.java
+++ Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/views/SchedulerView.java
@@ -1,10 +1,12 @@
 package samt.smajilbasic.views;
 
+import java.text.DateFormatSymbols;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.temporal.Temporal;
 import java.util.Calendar;
+import java.util.Locale;
 
 import com.vaadin.flow.component.AbstractField;
 import com.vaadin.flow.component.UI;
@@ -13,9 +15,11 @@ import com.vaadin.flow.component.HasValue.ValueChangeEvent;
 import com.vaadin.flow.component.HasValue.ValueChangeListener;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
 import com.vaadin.flow.component.timepicker.TimePicker;
@@ -23,6 +27,7 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.ui.CheckBox;
 
+import com.vaadin.ui.CheckBoxGroup;
 import samt.smajilbasic.Resources;
 import samt.smajilbasic.communication.Client;
 
@@ -31,6 +36,7 @@ import samt.smajilbasic.communication.Client;
  */
 @Route(value = "scheduler", layout = MainLayout.class)
 @PageTitle(value = "Deduplicator - Scheduler")
+@CssImport("./styles/radio-buttons-vertical.css")
 public class SchedulerView extends VerticalLayout {
 
     public static final String VIEW_NAME = "Scheduler";
@@ -39,55 +45,95 @@ public class SchedulerView extends VerticalLayout {
      * The HTTP/HTTPS client used for the communication.
      */
     private Client client;
+    private int weekNumber;
+    private int monthNumber;
 
     public SchedulerView() {
         super();
         client = (Client) UI.getCurrent().getSession().getAttribute(Resources.CURRENT_CLIENT_SESSION_ATTRIBUTE_KEY);
 
         if (client != null) {
-            Calendar calendar = Calendar.getInstance();
-            DatePicker datePicker = new DatePicker(LocalDate.now());
-            TimePicker timePicker = new TimePicker(LocalTime.now());
+            DatePicker datePicker = new DatePicker();
+            TimePicker timePicker = new TimePicker();
+
+            datePicker.setValue(LocalDate.now());
+            timePicker.setValue(LocalTime.now());
+
+            timePicker.setLocale(Locale.getDefault());
+            datePicker.setLocale(Locale.getDefault());
 
             ValueChangeListener listener = new ValueChangeListener<ValueChangeEvent<?>>() {
 
                 @Override
                 public void valueChanged(ValueChangeEvent<?> event) {
-                    if (datePicker.getValue().isBefore(LocalDate.now())) {
-                        Notification.show("Date can't be in the past").addThemeVariants(NotificationVariant.LUMO_ERROR);
+                    if (datePicker.getValue().isBefore(LocalDate.now()) && !datePicker.getValue().isEqual(LocalDate.now()) ) {
+                        Notification.show("Date can't be in the past", Resources.NOTIFICATION_LENGTH, Notification.Position.TOP_END).addThemeVariants(NotificationVariant.LUMO_ERROR);
                         datePicker.setValue(LocalDate.now());
                     } else {
-                        if (timePicker.getValue().isBefore(LocalTime.now())) {
-                            Notification.show("Time can't be in the past")
+                        if (timePicker.getValue().isBefore(LocalTime.now()) && !timePicker.getValue().isAfter(LocalTime.now()) ) {
+                            Notification.show("Time can't be in the past", Resources.NOTIFICATION_LENGTH, Notification.Position.TOP_END)
                                     .addThemeVariants(NotificationVariant.LUMO_ERROR);
                             timePicker.setValue(LocalTime.now());
                         }
                     }
+                    System.out.println("Value changed");
                 }
             };
 
             datePicker.addValueChangeListener(listener);
             timePicker.addValueChangeListener(listener);
 
+            HorizontalLayout pickerLayout = new HorizontalLayout(datePicker, timePicker);
+
             RadioButtonGroup<String> radioButtonGroup = new RadioButtonGroup<String>();
+            radioButtonGroup.setLabel("Select repetition frequency");
+
+            RadioButtonGroup<String> weekRadioButtonGroup = new RadioButtonGroup<String>();
+            weekRadioButtonGroup.setVisible(false);
+
+
+            String[] weekdayNames = DateFormatSymbols.getInstance().getWeekdays();
+            weekRadioButtonGroup.setItems(weekdayNames);
 
             radioButtonGroup.setItems("One off", "Daily", "Weekly", "Monthly");
+            radioButtonGroup.setValue("One off");
 
-            Button button = new Button("Set scan schedule", event -> {
-                client.insertSchedule(LocalDateTime.of(datePicker.getValue(), timePicker.getValue()),
-                        radioButtonGroup.getValue());
+
+            radioButtonGroup.addValueChangeListener(event -> {
+                        weekRadioButtonGroup.setVisible(false);
+                        if (event != null) {
+                            if (radioButtonGroup.getItemPosition(event.getValue()) == 2) {
+                                weekRadioButtonGroup.setVisible(true);
+                            } else {
+
+                            }
+
+                        }
+                    }
+            );
+
+
+            weekRadioButtonGroup.addValueChangeListener(event -> {
+                weekNumber = weekNumber & (1 << weekRadioButtonGroup.getItemPosition(event.getValue()));
+
+                System.out.println("Week Number: " + weekNumber);
             });
 
-            add(datePicker, radioButtonGroup, button);
+
+            Button button = new Button("Set scan schedule", event -> {
+
+//                client.insertSchedule(LocalDateTime.of(datePicker.getValue(), timePicker.getValue()),
+//                        radioButtonGroup.getValue());
+            });
+            add(datePicker, timePicker,radioButtonGroup,weekRadioButtonGroup,button);
         }
     }
 }
-
 // * @param monthly parametro da passare se lo {@link Scheduler} deve essere
 // * eseguito mensilmente.
 // * @param weekly parametro da passare se lo {@link Scheduler} deve essere
 // * eseguito settimanalmente.
-// * @param repeated parametro da passare se lo scheduler dovrà essere ripetuto,
+// * @param repeated parmetro da passare se lo scheduler dovrà essere ripetuto,
 // * valori: "true" o "false"
 // * @param timeStart la data e ora dalla quale partirà l'esecuzione formato
 // * timestamp -&gt; Long
