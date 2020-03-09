5aca88f7be22d311536d4f857ef679e4d91915dc
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2020-03-09 16:18:47 +0100

    Client: added addActions method

--- Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/communication/Client.java
+++ Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/communication/Client.java
@@ -1,8 +1,11 @@
 
 package samt.smajilbasic.communication;
 
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.jboss.resteasy.spi.HttpResponse;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -18,6 +21,8 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import samt.smajilbasic.Resources;
+import samt.smajilbasic.entity.Action;
 import samt.smajilbasic.entity.GlobalPath;
 
 import java.nio.charset.Charset;
@@ -34,12 +39,12 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Base64;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.net.ssl.SSLContext;
 
 /**
- *
  * @author Fadil Smajilbasic
  */
 public class Client {
@@ -71,7 +76,7 @@ public class Client {
     /**
      * Il costruttore che riceve il username e la password per l'autenticazione
      * basic che verrà utilizzata in tutte le GUI. Il costruttore tenta di caricare
-     * 
+     *
      * @param username il username da impostare.
      * @param password la password da impostare.
      */
@@ -97,7 +102,7 @@ public class Client {
          * authType) -> true; SSLContext sslContext = SSLContextBuilder.create()
          * .loadKeyMaterial(keyStore, CA_PASS.toCharArray()) .loadTrustMaterial(null,
          * acceptingTrustStrategy) .build();
-         * 
+         *
          * .setSSLContext(sslContext)
          */
         HttpClient httpClient = HttpClients.custom().build();
@@ -109,7 +114,7 @@ public class Client {
         /*
          * } catch (UnrecoverableKeyException | NoSuchAlgorithmException |
          * KeyStoreException | KeyManagementException e) {
-         * 
+         *
          * System.out.println("Unable to create client: " + e.getMessage());
          * e.printStackTrace(); }
          */
@@ -150,7 +155,7 @@ public class Client {
                     String.class, requestEntity);
 
             if (response.getStatusCode().equals(HttpStatus.OK)) {
-                
+
                 return response;
 
             } else {
@@ -229,7 +234,6 @@ public class Client {
         } catch (RestClientException rce) {
             System.out.println("Rest client exception: ");
             rce.printStackTrace(System.out);
-
         }
 
         return response;
@@ -315,22 +319,69 @@ public class Client {
         return response.getStatusCode();
     }
 
-	public HttpStatus insertSchedule(LocalDateTime dateTime, String repetition,int value) {
+    public Object insertSchedule(LocalDateTime dateTime,Integer weekNumber,Integer monthNumber,String repetition) {
 
         MultiValueMap<String, Object> values = new LinkedMultiValueMap<>();
-        if(repetition.length() > 0) {
-            if(repetition.equals("Daily"))
-            values.add("monthly", 0);
-            values.add("weekly", 0);
-            values.add("monthly", 0);
+        if (repetition.length() > 0) {
+            if(weekNumber != null)
+                values.add("weekly", weekNumber);
+            else
+                values.add("weekly","");
+            if(monthNumber != null)
+                values.add("monthly", monthNumber);
+            else
+                values.add("monthly",monthNumber);
         }
 
-        values.add("repeated", (!repetition.equals("One off")));
+        values.add("repeated", repetition.equals("One off"));
         values.add("timeStart", Timestamp.valueOf(dateTime).getTime());
 
 
-        ResponseEntity<String> response = put("scheduler/",values);
-        return HttpStatus.OK;
-	}
+        ResponseEntity<String> response = put("scheduler/", values);
+        if(response != null) {
+            if (response.getStatusCode() == HttpStatus.OK) {
+                return response.getBody();
+            } else {
+                return null;
+            }
+        }else{
+            return null;
+        }
+    }
 
+    public HttpStatus addActions(LocalDateTime time, List<GlobalPath> actions) {
+
+        Object response = insertSchedule(time,null,null, "One off");
+        if (response != null) {
+
+            try {
+                JSONObject responseJSON = (JSONObject) parser.parse(response.toString());
+                String schedulerId = responseJSON.get("id").toString();
+                for (GlobalPath path : actions) {
+                    Action action = path.getAction();
+                    MultiValueMap<String, Object> values = new LinkedMultiValueMap<String, Object>();
+
+                    values.add("type", action.getType().toString());
+                    values.add("path", path.getPath());
+                    values.add("newPath", action.getNewPath());
+                    values.add("scheduler", schedulerId);
+
+
+                    ResponseEntity<String> addResponse = put("action/", values);
+                    if (addResponse.getStatusCode() == HttpStatus.INTERNAL_SERVER_ERROR) {
+                        Notification.show("Unable to add action of: " + path.getPath(), Resources.NOTIFICATION_LENGTH, Notification.Position.TOP_END).addThemeVariants(NotificationVariant.LUMO_ERROR);
+                    }
+                }
+                return HttpStatus.OK;
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+                Notification.show("Unable to parse server response", Resources.NOTIFICATION_LENGTH, Notification.Position.TOP_END).addThemeVariants(NotificationVariant.LUMO_ERROR);
+                return HttpStatus.BAD_REQUEST;
+            }
+        } else {
+            Notification.show("Unable to create scheduler to add actions", Resources.NOTIFICATION_LENGTH, Notification.Position.TOP_END).addThemeVariants(NotificationVariant.LUMO_ERROR);
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+    }
 }
