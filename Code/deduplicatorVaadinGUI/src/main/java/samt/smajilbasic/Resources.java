c91b0ee7c51657d2304cfbca46c312177c74bf78
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2020-03-16 15:16:56 +0100

    Added screen sizes to Resource class

--- Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/Resources.java
+++ Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/Resources.java
@@ -19,4 +19,11 @@ public abstract class Resources {
     public static final String CURRENT_USER_SESSION_ATTRIBUTE_KEY = CurrentUser.class.getCanonicalName();
 
     public static final String CURRENT_CLIENT_SESSION_ATTRIBUTE_KEY = Client.class.getCanonicalName();
+
+    public static final String SIZE_MOBILE_L = "425px";
+    public static final String SIZE_MOBILE_M = "375px";
+    public static final String SIZE_MOBILE_S = "320px";
+    public static final String SIZE_TABLET = "768px";
+    public static final String SIZE_LAPTOP = "1024px";
+
 }
