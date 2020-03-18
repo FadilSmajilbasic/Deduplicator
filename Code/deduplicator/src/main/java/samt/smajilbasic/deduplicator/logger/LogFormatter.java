ae066623f080c5c51599a36b682f59a9408bf955
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2020-03-18 22:41:55 +0100

    Changed log format

--- Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/logger/LogFormatter.java
+++ Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/logger/LogFormatter.java
@@ -3,6 +3,7 @@ package samt.smajilbasic.deduplicator.logger;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.logging.Formatter;
+import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 /**
@@ -14,20 +15,20 @@ public class LogFormatter extends Formatter {
     public String format(LogRecord record) {
         StringBuilder builder = new StringBuilder();
 
-        builder.append("[" + record.getLevel().getName() +"] ");
-        
+        builder.append("[" + record.getLevel().getName() + "] ");
+
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSS");
         Calendar cal = Calendar.getInstance();
         cal.setTimeInMillis(record.getMillis());
-        builder.append(sdf.format(cal.getTime())+ " ");
-        
-        builder.append(record.getMessage()+" at ");
-        builder.append(record.getSourceClassName() + " at " + record.getSourceMethodName());
+        builder.append(sdf.format(cal.getTime()) + " ");
+
+        builder.append(record.getMessage());
+        if (record.getLevel().equals(Level.SEVERE)) {
+            builder.append(" at " + record.getSourceClassName() + " at " + record.getSourceMethodName());
+        }
+        builder.append("\n");
 
         return builder.toString();
     }
 
-    
-    
-    
 }
