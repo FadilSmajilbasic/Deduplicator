693892c3f8d27759dfc4730ac4a81bb74cf2b83f
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2020-03-18 22:41:16 +0100

    Added call to "/logout" to complete logout process

--- Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/authentication/AccessControl.java
+++ Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/authentication/AccessControl.java
@@ -3,6 +3,7 @@ package samt.smajilbasic.authentication;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.server.VaadinSession;
 
+import samt.smajilbasic.Resources;
 import samt.smajilbasic.communication.Client;
 
 /**
@@ -24,6 +25,9 @@ public class AccessControl implements AccessControlInterface {
     public void signOut() {
         VaadinSession.getCurrent().getSession().invalidate();
         CurrentUser.set(null, null);
+        Client client = (Client) UI.getCurrent().getSession().getAttribute(Resources.CURRENT_CLIENT_SESSION_ATTRIBUTE_KEY);
+        client.post("/logout", null);
+        System.out.println("Logged out from client");
         UI.getCurrent().navigate("");
     }
 
