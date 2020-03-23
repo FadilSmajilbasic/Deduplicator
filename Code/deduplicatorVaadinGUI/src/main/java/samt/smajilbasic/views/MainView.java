c28bb3d0d7c7cb552b031cc459f22cfc7dfe199c
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2020-03-23 18:35:06 +0100

    Added last report to main view

--- Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/views/MainView.java
+++ Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/views/MainView.java
@@ -1,11 +1,30 @@
 package samt.smajilbasic.views;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.Command;
+import com.vaadin.flow.shared.communication.PushMode;
+import com.vaadin.flow.shared.ui.Transport;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.http.ResponseEntity;
+import samt.smajilbasic.communication.Client;
 import samt.smajilbasic.model.Resources;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 /**
  * MainView
@@ -13,14 +32,38 @@ import samt.smajilbasic.model.Resources;
 @Route(value = "", layout = MainLayout.class)
 @CssImport("./styles/shared-styles.css")
 @PageTitle(value = "Deduplicator - Home")
-public class MainView extends VerticalLayout{
+public class MainView extends VerticalLayout {
 
     public static final String VIEW_NAME = "Home";
+    private Client client;
+
+    /**
+     * The parser used to read the JSON response from the server when updating the
+     * status.
+     */
+    private JSONParser parser = new JSONParser();
 
 	public MainView() {
-        super();
+        this.setSizeFull();
         this.setAlignItems(Alignment.CENTER);
-        add(new Label("Welcome to Main View"),new Label("Select a section from the side menu"));
+        add(new Label("Welcome to Main View"),new Label("Select a section from the side menu"),new Label("The results of the last scan:"));
         setMinWidth(Resources.SIZE_MOBILE_S);
+        client = (Client) UI.getCurrent().getSession().getAttribute(Resources.CURRENT_CLIENT_SESSION_ATTRIBUTE_KEY);
+        UI ui = UI.getCurrent();
+        ui.getPushConfiguration().setPushMode(PushMode.MANUAL);
+        ui.getPushConfiguration().setTransport(Transport.WEBSOCKET);
+        Command updateTable = new Command() {
+            @Override
+            public void execute() {
+                if (client != null) {
+                    ReportView report = new ReportView(true);
+                    report.setSizeFull();
+                    add(report);
+                }
+            }
+        };
+        ui.accessSynchronously(updateTable); // TODO: fix synchronous access
     }
+
+
 }
