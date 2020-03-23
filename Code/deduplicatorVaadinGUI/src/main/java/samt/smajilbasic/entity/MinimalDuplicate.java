ece007eeef42283a1cd291d2ad6176eb2466baf2
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2020-03-23 17:34:12 +0100

    Extracted MinimalDuplicate class from ReportView

---
+++ Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/entity/MinimalDuplicate.java
@@ -0,0 +1,54 @@
+package samt.smajilbasic.entity;
+
+/**
+ * Class that describes the duplicate object returned by the API when querying
+ * all duplicates of a report.
+ */
+public class MinimalDuplicate {
+    private String hash;
+    private String size;
+    private String count;
+
+    /**
+     * @return the hash
+     */
+    public String getHash() {
+        return hash;
+    }
+
+    /**
+     * @param hash the hash to set
+     */
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    /**
+     * @return the size
+     */
+    public String getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    /**
+     * @return the count
+     */
+    public String getCount() {
+        return count;
+    }
+
+    /**
+     * @param count the count to set
+     */
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+}
