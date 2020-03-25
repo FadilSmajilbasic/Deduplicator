3ea4c0f2f3fdbfb96514c2fc04142e8f9b0bba2a
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2020-03-26 00:36:17 +0100

    GlobalPath: changed date to lastModified

--- Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/entity/GlobalPath.java
+++ Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/entity/GlobalPath.java
@@ -3,142 +3,144 @@ package samt.smajilbasic.entity;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * La classe GlobalPath descrive un percorso inserito dall'utente per la
  * scansione o per essere escluso dalla scansione.
  */
 public class GlobalPath {
-	/**
-	 * L'attributo path conteine il percorso assoluto del file o cartella. Utilizza
-	 * l'annotazione @Id per indicare che è una chiave primaria della tabella.
-	 */
-	private String path = "";
+    /**
+     * L'attributo path conteine il percorso assoluto del file o cartella. Utilizza
+     * l'annotazione @Id per indicare che è una chiave primaria della tabella.
+     */
+    private String path = "";
 
-	/**
-	 * L'attributo file indica se il percorso {@link GlobalPath#path} sia un file
-	 * oppure una directory.
-	 */
-	private Boolean file = false;
+    /**
+     * L'attributo file indica se il percorso {@link GlobalPath#path} sia un file
+     * oppure una directory.
+     */
+    private Boolean file = false;
 
-	/**
-	 * L'attributo ignoreFile indica se l'oggetto è da ignorare o se deve essere
-	 * preso in considerazione durante la scansione.
-	 */
-	private Boolean ignoreFile = false;
+    /**
+     * L'attributo ignoreFile indica se l'oggetto è da ignorare o se deve essere
+     * preso in considerazione durante la scansione.
+     */
+    private Boolean ignoreFile = false;
 
-	/**
-	 * L'attributo date indica la data d'aggiunta in formato timestamp.
-	 */
-	private Long date = 0L;
+    /**
+     * lastModified is the date that the file was last modified on the filesystem, it is saved as a timestamp
+     */
+    private Long lastModified = 0L;
 
-	private Action action;
+    private Action action;
 
-	/**
-	 * Costruttore vuoto.
-	 */
-	public GlobalPath() {
-	}
+    /**
+     * Costruttore vuoto.
+     */
+    public GlobalPath() {
+    }
 
-	/**
-	 * Costruttore che accetta il percorso dell'oggetto e se è da ignorare oppure
-	 * no.
-	 * 
-	 * @param path       il percorso del oggetto.
-	 * @param ignoreFile se è da ignorare o no.
-	 */
-	public GlobalPath(String path, boolean ignoreFile, Long date) {
-		this.setPath(path);
-		this.setignoreFile(ignoreFile);
-		this.setDate(date);
-	}
+    /**
+     * Costruttore che accetta il percorso dell'oggetto e se è da ignorare oppure
+     * no.
+     *
+     * @param path       il percorso del oggetto.
+     * @param ignoreFile se è da ignorare o no.
+     */
+    public GlobalPath(String path, boolean ignoreFile, Long lastModified) {
+        this.setPath(path);
+        this.setignoreFile(ignoreFile);
+        this.setLastModified(lastModified);
+    }
 
-	/**
-	 * Metodo getter per la variabile path..
-	 * 
-	 * @return il percorso del oggetto.
-	 */
-	public String getPath() {
-		return path;
-	}
+    /**
+     * Metodo getter per la variabile path..
+     *
+     * @return il percorso del oggetto.
+     */
+    public String getPath() {
+        return path;
+    }
 
-	/**
-	 * Metodo setter per la variabile path. Se il percorso non è valido viene tirata
-	 * una RuntimeException.
-	 * 
-	 * @param path il percorso da impostare.
-	 */
-	private void setPath(String path) {
-		this.path = path;
-	}
+    /**
+     * Metodo setter per la variabile path. Se il percorso non è valido viene tirata
+     * una RuntimeException.
+     *
+     * @param path il percorso da impostare.
+     */
+    private void setPath(String path) {
+        this.path = path;
+    }
 
-	/**
-	 * Metodo getter per la variabile file.
-	 * 
-	 * @return true se il percorso impostato è un file, false se è una cartella.
-	 */
-	public boolean isFile() {
-		return file;
-	}
+    /**
+     * Metodo getter per la variabile file.
+     *
+     * @return true se il percorso impostato è un file, false se è una cartella.
+     */
+    public boolean isFile() {
+        return file;
+    }
 
-	/**
-	 * Metodo setter per la variabile file.
-	 * 
-	 * @param file true se il percorso impostato è un file, false altrimenti.
-	 */
-	private void setFile(boolean file) {
-		this.file = file;
-	}
+    /**
+     * Metodo setter per la variabile file.
+     *
+     * @param file true se il percorso impostato è un file, false altrimenti.
+     */
+    private void setFile(boolean file) {
+        this.file = file;
+    }
 
-	/**
-	 * Metodo getter per la variabile ignoreFile.
-	 * 
-	 * @return true se l'oggetto è impostato per essere ignorato, false altrimenti.
-	 */
-	public boolean isignoreFile() {
-		return ignoreFile;
-	}
+    /**
+     * Metodo getter per la variabile ignoreFile.
+     *
+     * @return true se l'oggetto è impostato per essere ignorato, false altrimenti.
+     */
+    public boolean isignoreFile() {
+        return ignoreFile;
+    }
 
-	/**
-	 * Metodo setter per la variabile ignoreFile
-	 * 
-	 * @param ignoreFile true se il l'oggetto deve essere ignorato, false se deve
-	 *                   essere preso in considerazione durante la scansione.
-	 */
-	private void setignoreFile(boolean ignoreFile) {
-		this.ignoreFile = ignoreFile;
-	}
+    /**
+     * Metodo setter per la variabile ignoreFile
+     *
+     * @param ignoreFile true se il l'oggetto deve essere ignorato, false se deve
+     *                   essere preso in considerazione durante la scansione.
+     */
+    private void setignoreFile(boolean ignoreFile) {
+        this.ignoreFile = ignoreFile;
+    }
 
-	/**
-	 * Metodo getter per la variabile date.
-	 * 
-	 * @return la data in formato timestamp.
-	 */
-	public Long getDate() {
-		return date;
-	}
+    /**
+     * Metodo getter per la variabile date.
+     *
+     * @return la data in formato timestamp.
+     */
+    public Long getLastModified() {
+        return lastModified;
+    }
 
-	public String getDateFormatted() {
-		DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
-		Calendar cal = Calendar.getInstance();
-		cal.setTimeInMillis(getDate());
-		return dateFormat.format(cal.getTime());
-	}
+    public String getDateFormatted() {
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(getLastModified());
+        return dateFormat.format(cal.getTime());
+    }
 
-	/**
-	 * Metodo setter per la variabile date.
-	 * 
-	 * @param date la data da impostare in formato timestamp.
-	 */
-	private void setDate(Long date) {
-		this.date = date;
-	}
+    /**
+     * Metodo setter per la variabile date.
+     *
+     * @param lastModified la data da impostare in formato timestamp.
+     */
+    private void setLastModified(Long lastModified) {
+        this.lastModified = lastModified;
+    }
 
-	public Action getAction() {
-		return action;
-	}
+    public Action getAction() {
+        return action;
+    }
 
-	public void setAction(Action action) {
-		this.action = action;
-	}
+    public void setAction(Action action) {
+        this.action = action;
+    }
 }
