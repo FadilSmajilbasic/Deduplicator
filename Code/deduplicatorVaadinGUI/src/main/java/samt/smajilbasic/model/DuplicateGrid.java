4b2d3169f76b412653f305e20449aff4efba05eb
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2020-03-26 00:33:52 +0100

    Updated DuplicateGrid style

--- Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/model/DuplicateGrid.java
+++ Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/model/DuplicateGrid.java
@@ -1,7 +1,9 @@
 package samt.smajilbasic.model;
 
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import samt.smajilbasic.entity.GlobalPath;
 import samt.smajilbasic.entity.MinimalDuplicate;
@@ -14,19 +16,19 @@ public class DuplicateGrid {
     private Grid<GlobalPath> item;
     private MinimalDuplicate duplicate;
 
-    public DuplicateGrid(List<GlobalPath> paths,MinimalDuplicate duplicate){
+    public DuplicateGrid(List<GlobalPath> paths,MinimalDuplicate duplicate,boolean forMainView){
         Grid<GlobalPath> grid = new Grid<GlobalPath>();
         grid.setItems(paths);
-
-        grid.addColumn(GlobalPath::getPath).setHeader("Path").setFlexGrow(2);
+        grid.addColumn(GlobalPath::getPath).setHeader("Path").setFlexGrow(6);
         grid.addColumn(new ComponentRenderer<Label,GlobalPath>((dateLabel ->{
-            System.out.println("date: " + dateLabel.getDate());
-            return new Label(dateLabel.getDateFormatted()); // TODO: Fix date label
-        }))).setHeader("Date modified")
-            .setFlexGrow(1);
-
-        grid.addColumn(new ComponentRenderer<>(item -> new DuplicatesButtonLayout(item))).setHeader("Manage").setFlexGrow(1);
+            return new Label(dateLabel.getDateFormatted());
+        }))).setHeader(new Label("Date modified")).setFlexGrow(1);
+        if(!forMainView)
+            grid.addColumn(new ComponentRenderer<FormLayout,GlobalPath>(DuplicatesButtonLayout::new)).setHeader("Manage").setFlexGrow(2);
         grid.setHeightByRows(true);
+        grid.setSelectionMode(Grid.SelectionMode.NONE);
+        grid.setSizeFull();
+        grid.setClassName("inside-grid");
         item = grid;
         this.duplicate = duplicate;
     }
