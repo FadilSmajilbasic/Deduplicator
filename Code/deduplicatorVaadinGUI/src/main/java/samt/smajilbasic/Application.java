916d1bd798f1a58a585baf9b6a9de278dcb7afed
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2020-03-26 00:37:21 +0100

    GUI project: added logger setup on application start

--- Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/Application.java
+++ Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/Application.java
@@ -3,6 +3,10 @@ package samt.smajilbasic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import samt.smajilbasic.logger.MyLogger;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
 
 /**
  * The entry point of the Spring Boot application.
@@ -12,6 +16,16 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void startup(){
+        try{
+            MyLogger.setup();
+        }catch(IOException ioe){
+            System.out.println("Unable to setup logger");
+            ioe.printStackTrace();
+        }
     }
 
 }
