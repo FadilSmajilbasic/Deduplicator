458b04378063961645ed4847b64b3b4de695023d
FadilSmajilbasic <smajilbaicfadil@gmail.com>
2020-03-31 01:37:04 +0200

    AccountController: updated usernameUpdate,  userInsert and passwordUpdate method

--- Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/controller/AccountController.java
+++ Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/controller/AccountController.java
@@ -10,6 +10,7 @@ import samt.smajilbasic.deduplicator.repository.AuthenticationDetailsRepository;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.Security;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -33,7 +34,7 @@ import org.springframework.web.servlet.ModelAndView;
  * primo pezzo del percorso "/account". Usa l'annotazione @RestController per
  * indicare a spring che questa classe è un controller e che dovrà essere
  * inizializzata all'avvio dell'applicazione.
- * 
+ *
  * @author Fadil Smajilbasic
  */
 @RestController
@@ -72,13 +73,13 @@ public class AccountController {
     /**
      * Il metodo insert risponde alla richiesta di tipo PUT sull'indirizzo
      * <b>&lt;indirizzo-server&gt;/login/user</b>(localhost:8080/login/user/).
-     * 
+     *
      * @param username il username del utente da inserire, il parametro deve essere
      *                 nella parte body della richiesta.
      * @param password la password del utente da inserire, il parametro deve essere
      *                 nella parte body della richiesta.
      * @return L'utente inserito nel database oppure un messaggio d'errore in base
-     *         al errore riscontrato.
+     * al errore riscontrato.
      */
     @PutMapping()
     public Object insert(@RequestParam String username, @RequestParam String password) {
@@ -88,88 +89,95 @@ public class AccountController {
             if (!password.equals("") && password.length() >= AccessController.PASSWORD_LENGTH) {
                 if (!adr.existsById(username)) {
                     try {
-                        adr.save(new AuthenticationDetails(username, password));
+                        AuthenticationDetails ad = new AuthenticationDetails(username, password);
+                        adr.save(ad);
+                        User.UserBuilder builder = User.builder();
+
+                        inMemoryUserDetailsManager.createUser(builder.username(username).password(ad.getPassword()).roles("USER").build());
                         return adr.findById(username).get();
                     } catch (NoSuchAlgorithmException e) {
+                        Logger.getGlobal().log(Level.SEVERE, "BCrypt algorithm not available on server");
                         return new ResponseEntity<Response>(new Response("BCrypt algorithm not available on server"),
-                                HttpStatus.INTERNAL_SERVER_ERROR);
-
+                            HttpStatus.INTERNAL_SERVER_ERROR);
                     }
                 } else {
+                    Logger.getGlobal().log(Level.SEVERE, "Username already taken");
                     return new ResponseEntity<Response>(new Response("Username already taken"),
-                            HttpStatus.INTERNAL_SERVER_ERROR);
-
+                        HttpStatus.INTERNAL_SERVER_ERROR);
                 }
             } else {
+                Logger.getGlobal().log(Level.SEVERE, "Password too short: " + password);
+
                 return new ResponseEntity<Response>(new Response("Password too short, should be at least "
-                        + AccessController.PASSWORD_LENGTH + " charaters long"), HttpStatus.BAD_REQUEST);
+                    + AccessController.PASSWORD_LENGTH + " charaters long"), HttpStatus.BAD_REQUEST);
 
             }
         } else {
+            Logger.getGlobal().log(Level.SEVERE, "Username too short: " + username);
             return new ResponseEntity<Response>(new Response(
-                    "Username too short, should be at least" + AccessController.USERNAME_LENGTH + "charaters long"),
-                    HttpStatus.BAD_REQUEST);
+                "Username too short, should be at least" + AccessController.USERNAME_LENGTH + "characters long"),
+                HttpStatus.BAD_REQUEST);
 
         }
 
     }
 
     @PutMapping("/password")
-    public Object updatePassword( @RequestParam String oldPassword,@RequestParam String newPassword) {
+    public Object updatePassword(@RequestParam String oldPassword, @RequestParam String newPassword) {
         if (!oldPassword.isBlank()) {
             if (!newPassword.isBlank()) {
                 if (newPassword.length() >= AccessController.PASSWORD_LENGTH) {
                     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-                    AuthenticationDetails internalUser = adr.findById(authentication.getName()).get();
-                    if (internalUser != null) {
-
+                    Optional<AuthenticationDetails> tempUser = adr.findById(authentication.getName());
+                    if (tempUser.isPresent()) {
+                        AuthenticationDetails internalUser = tempUser.get();
                         if (encoder.matches(oldPassword, internalUser.getPassword())) {
                             try {
-                                internalUser = new AuthenticationDetails(internalUser.getUsername(),newPassword);
+                                internalUser = new AuthenticationDetails(internalUser.getUsername(), newPassword);
                                 adr.save(internalUser);
-                                if(updatePasswordInMemory(internalUser.getUsername(),internalUser.getPassword())) {
+                                if (updatePasswordInMemory(internalUser.getUsername(), internalUser.getPassword())) {
                                     Logger.getGlobal().log(Level.INFO,
-                                            "Password updated successfully for user " + internalUser.getUsername());
+                                        "Password updated successfully for user " + internalUser.getUsername());
                                     return new ResponseEntity<>(HttpStatus.OK);
-                                }else{
+                                } else {
                                     Logger.getGlobal().log(Level.SEVERE,
-                                            "An error occurred while updating the password of " + internalUser.getUsername());
-                                    return new ResponseEntity<Response>(new Response("An error occurred while updating the password"),HttpStatus.INTERNAL_SERVER_ERROR);
+                                        "An error occurred while updating the password of " + internalUser.getUsername());
+                                    return new ResponseEntity<Response>(new Response("An error occurred while updating the password"), HttpStatus.INTERNAL_SERVER_ERROR);
                                 }
                             } catch (NoSuchAlgorithmException e) {
                                 Logger.getGlobal().log(Level.SEVERE, "BCrypt algorithm not available on server");
 
                                 return new ResponseEntity<Response>(
-                                        new Response("BCrypt algorithm not available on server"),
-                                        HttpStatus.INTERNAL_SERVER_ERROR);
+                                    new Response("BCrypt algorithm not available on server"),
+                                    HttpStatus.INTERNAL_SERVER_ERROR);
                             }
                         } else {
                             Logger.getGlobal().log(Level.WARNING,
-                                    "oldPassword parameter doesn't match the current password");
+                                "oldPassword parameter doesn't match the current password");
                             System.out.println(
-                                    "oldPassword: " + oldPassword + " internal: " + internalUser.getPassword());
+                                "oldPassword: " + oldPassword + " internal: " + internalUser.getPassword());
                             return new ResponseEntity<Response>(
-                                    new Response("oldPassword parameter doesn't match the current password"),
-                                    HttpStatus.INTERNAL_SERVER_ERROR);
+                                new Response("oldPassword parameter doesn't match the current password"),
+                                HttpStatus.INTERNAL_SERVER_ERROR);
                         }
                     } else {
                         Logger.getGlobal().log(Level.WARNING, "Invalid user in database");
 
                         return new ResponseEntity<Response>(new Response("Invalid user in database"),
-                                HttpStatus.INTERNAL_SERVER_ERROR);
+                            HttpStatus.INTERNAL_SERVER_ERROR);
                     }
                 } else {
                     Logger.getGlobal().log(Level.WARNING, "New password too short");
                     return new ResponseEntity<Response>(
-                            new Response("New password too short, please provide a password of at least "
-                                    + AccessController.PASSWORD_LENGTH + " characters."),
-                            HttpStatus.INTERNAL_SERVER_ERROR);
+                        new Response("New password too short, please provide a password of at least "
+                            + AccessController.PASSWORD_LENGTH + " characters."),
+                        HttpStatus.INTERNAL_SERVER_ERROR);
                 }
 
             } else {
                 Logger.getGlobal().log(Level.WARNING, "New password invalid");
                 return new ResponseEntity<Response>(new Response("New password invalid"),
-                        HttpStatus.INTERNAL_SERVER_ERROR);
+                    HttpStatus.INTERNAL_SERVER_ERROR);
             }
         } else {
             Logger.getGlobal().log(Level.WARNING, "Old password invalid");
@@ -179,7 +187,7 @@ public class AccountController {
     }
 
     @PutMapping("/username")
-    public Object updateUsername(@RequestParam String newUsername, @RequestParam String password) {
+    public ResponseEntity<?> updateUsername(@RequestParam String newUsername, @RequestParam String password) {
         if (!newUsername.isBlank()) {
             if (newUsername.length() >= AccessController.USERNAME_LENGTH) {
                 if (!password.isBlank()) {
@@ -189,32 +197,38 @@ public class AccountController {
                         if (encoder.matches(password, internalUser.getPassword())) {
                             delete(internalUser.getUsername());
                             insert(newUsername, password);
-                            Logger.getGlobal().log(Level.INFO, "Username updated successfully from "
+                            if (updateUsernameInMemory(internalUser.getUsername(), newUsername)) {
+                                Logger.getGlobal().log(Level.INFO, "Username updated successfully from "
                                     + internalUser.getUsername() + " to " + newUsername);
-                            return new ResponseEntity(HttpStatus.OK);
+                                return new ResponseEntity<String>(HttpStatus.OK);
+                            } else {
+                                Logger.getGlobal().log(Level.SEVERE,
+                                    "An error occurred while updating the username of " + internalUser.getUsername());
+                                return new ResponseEntity<Response>(new Response("An error occurred while updating the username of " + internalUser.getUsername()), HttpStatus.INTERNAL_SERVER_ERROR);
+                            }
                         } else {
                             Logger.getGlobal().log(Level.WARNING, "Invalid user in database");
                             return new ResponseEntity<Response>(new Response("Invalid user in database"),
-                                    HttpStatus.INTERNAL_SERVER_ERROR);
+                                HttpStatus.INTERNAL_SERVER_ERROR);
                         }
                     } else {
                         Logger.getGlobal().log(Level.WARNING, "Unable to change username of admin");
                         return new ResponseEntity<Response>(new Response("Unable to change username of admin"),
-                                HttpStatus.INTERNAL_SERVER_ERROR);
+                            HttpStatus.INTERNAL_SERVER_ERROR);
                     }
 
                 } else {
                     Logger.getGlobal().log(Level.WARNING, "Password invalid");
 
                     return new ResponseEntity<Response>(new Response("Password invalid"),
-                            HttpStatus.INTERNAL_SERVER_ERROR);
+                        HttpStatus.INTERNAL_SERVER_ERROR);
                 }
             } else {
                 Logger.getGlobal().log(Level.WARNING, "Username too short");
 
                 return new ResponseEntity<Response>(new Response(
-                        "Username too short, should be at least" + AccessController.USERNAME_LENGTH + "charaters long"),
-                        HttpStatus.BAD_REQUEST);
+                    "Username too short, should be at least" + AccessController.USERNAME_LENGTH + "charaters long"),
+                    HttpStatus.BAD_REQUEST);
             }
         } else {
             Logger.getGlobal().log(Level.WARNING, "New username invalid");
@@ -227,11 +241,11 @@ public class AccountController {
     /**
      * Il metodo insertUser risponde alla richiesta di tipo DELETE sull'indirizzo
      * <b>&lt;indirizzo-server&gt;/login/user</b>(localhost:8080/login/user/).
-     * 
+     *
      * @param username il username del utente da eliminare, il parametro deve essere
      *                 nella parte body della richiesta.
      * @return L'utente eliminato oppure il messaggio d'errore se l'utente da
-     *         eliminare non esiste.
+     * eliminare non esiste.
      */
     @DeleteMapping()
     public Object delete(@RequestParam String username) {
@@ -244,19 +258,21 @@ public class AccountController {
             if (internalUser.getUsername().equals("admin")) {
                 if (!username.equals("admin")) {
                     if (!username.equals(internalUser.getUsername())) {
-                        adr.delete(adr.findById(username).get());
-                        return adr.findById(username).get();
+                        AuthenticationDetails user = adr.findById(username).get();
+                        adr.delete(user);
+                        inMemoryUserDetailsManager.deleteUser(username);
+                        return user;
                     } else {
                         return new ResponseEntity<Response>(new Response("Cannot delete current user" + username),
-                                HttpStatus.INTERNAL_SERVER_ERROR);
+                            HttpStatus.INTERNAL_SERVER_ERROR);
                     }
                 } else {
                     return new ResponseEntity<Response>(new Response("Admin cannot be deleted" + username),
-                            HttpStatus.INTERNAL_SERVER_ERROR);
+                        HttpStatus.INTERNAL_SERVER_ERROR);
                 }
             } else {
                 return new ResponseEntity<Response>(new Response("Only admin can delete users" + username),
-                        HttpStatus.UNAUTHORIZED);
+                    HttpStatus.UNAUTHORIZED);
             }
         } else {
             return new ResponseEntity<Response>(new Response("No user with username" + username), HttpStatus.NOT_FOUND);
@@ -264,26 +280,25 @@ public class AccountController {
 
     }
 
-
     private boolean exists(String username) {
         return inMemoryUserDetailsManager.userExists(username);
     }
 
-    private boolean updatePasswordInMemory(String username,String password){
-        if(exists(username)) {
+    private boolean updatePasswordInMemory(String username, String password) {
+        if (exists(username)) {
             inMemoryUserDetailsManager.changePassword(inMemoryUserDetailsManager.loadUserByUsername(username).getPassword(), password);
             return true;
-        }else{
-            Logger.getGlobal().log(Level.SEVERE,"Unable to update password of nonexistent user: " + username);
+        } else {
+            Logger.getGlobal().log(Level.SEVERE, "Unable to update password of nonexistent user: " + username);
             return false;
         }
     }
 
-    private boolean updateUsernameInMemory(String username,String newUsername){
-        if(exists(username)) {
-            UserDetails user =  inMemoryUserDetailsManager.loadUserByUsername(username);
-            if(deleteUser(username)) {
-                Function<String,String> passwordEncoder = new Function<String, String>() {
+    private boolean updateUsernameInMemory(String username, String newUsername) {
+        if (exists(username)) {
+            UserDetails user = inMemoryUserDetailsManager.loadUserByUsername(username);
+            if (deleteUser(username)) {
+                Function<String, String> passwordEncoder = new Function<String, String>() {
                     @Override
                     public String apply(String password) {
                         return new BCryptPasswordEncoder().encode(password);
@@ -291,23 +306,23 @@ public class AccountController {
                 };
                 inMemoryUserDetailsManager.createUser(User.builder().username(username).passwordEncoder(passwordEncoder).password(user.getPassword()).roles("USER").build());
                 return true;
-            }else{
-                Logger.getGlobal().log(Level.SEVERE,"Unable to delete user");
+            } else {
+                Logger.getGlobal().log(Level.SEVERE, "Unable to delete user");
                 return false;
             }
-
-        }else{
-            Logger.getGlobal().log(Level.SEVERE,"Unable to update username of nonexistent user: " + username);
+        } else {
+            Logger.getGlobal().log(Level.SEVERE, "Unable to update username of nonexistent user: " + username);
             return false;
         }
     }
-    private boolean deleteUser(String username){
-        if(exists(username)){
+
+    private boolean deleteUser(String username) {
+        if (exists(username)) {
             inMemoryUserDetailsManager.deleteUser(username);
-            Logger.getGlobal().log(Level.INFO,"Successfully deleted user " + username);
+            Logger.getGlobal().log(Level.INFO, "Successfully deleted user " + username);
             return true;
-        }else{
-            Logger.getGlobal().log(Level.SEVERE,"Unable to delete user");
+        } else {
+            Logger.getGlobal().log(Level.SEVERE, "Unable to delete user");
             return false;
         }
     }
