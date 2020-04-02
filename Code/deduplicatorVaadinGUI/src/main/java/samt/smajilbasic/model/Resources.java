59f1bf89ab4ab520680ba32fce319ce0df315011
FadilSmajilbasic <smajilbaicfadil@gmail.com>
2020-04-03 00:58:56 +0200

    Resources: Set default properties and removed ConfigurationProperties field

--- Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/model/Resources.java
+++ Code/deduplicatorVaadinGUI/src/main/java/samt/smajilbasic/model/Resources.java
@@ -1,21 +1,20 @@
 package samt.smajilbasic.model;
 
 import org.springframework.stereotype.Component;
-import samt.smajilbasic.SpringContext;
 import samt.smajilbasic.authentication.CurrentUser;
 import samt.smajilbasic.communication.Client;
-import samt.smajilbasic.configuration.ConfigProperties;
 
 /**
  * Resources
  */
 @Component
-public class Resources {
+public abstract class Resources {
 
     /**
      * Defines the default notification length in ms.
      */
-    public final int notificationLength;
+    public static final int NOTIFICATION_LENGTH = 2000;
+    public static final int REFRESH_INTERVAL = 500;
     
     /**
      * The key used to store the reference to the username in the session.
@@ -32,20 +31,11 @@ public class Resources {
     public static final String SIZE_LAPTOP_L = "1440px";
     public static final String SIZE_LAPTOP_4K = "2560px";
 
-    //TODO: check resources taht can be exported to applciation.properties
 
     public static final int USERNAME_LENGTH = 4;
     public static final int PASSWORD_LENGTH = 8;
 
     public static final int DUPLICATES_BUFFER_LENGTH = 10;
 
-    private ConfigProperties props = SpringContext.getBean(ConfigProperties.class);
-
-    public Resources(){
-        notificationLength = Integer.parseInt(props.getNotificationLength());
-    }
-
-    public int getNotificationLength() {
-        return notificationLength;
-    }
+    public static final String LOG_PATH = "log/";
 }
