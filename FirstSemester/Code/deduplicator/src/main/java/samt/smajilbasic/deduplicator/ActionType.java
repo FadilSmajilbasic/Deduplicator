6331fe8f0d16cd748158f3067d66f16da3faa57c
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2020-04-06 07:18:07 +0200

    Modified folder structure of the whole project

--- FirstSemester/Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/ActionType.java
+++
@@ -1,8 +0,0 @@
-package samt.smajilbasic.deduplicator;
-
-public interface ActionType {
-    public static String  DELETE = "DELETE";
-    public static String  MOVE = "MOVE";
-    public static String  IGNORE = "IGNORE";
-    public static String  SCAN = "SCAN";
-}
--- FirstSemester/Code/deduplicator/src/main/java/samt/smajilbasic/deduplicator/scanner/ScanListener.java
+++
@@ -1,8 +0,0 @@
-package samt.smajilbasic.deduplicator.scanner;
-
-/**
- * L'interfaccia ScanListener definisce un listener che aspetta la fine d'esecuione del {@link ScannerThread}. 
- */
-public interface ScanListener {
-    public void scanFinished();
-}
